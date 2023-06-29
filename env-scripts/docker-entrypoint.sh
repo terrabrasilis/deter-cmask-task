2b1ad2997d5d9990dd435f85481997ae89eca778
andre-carvalho <afacarvalho@yahoo.com.br>
2023-06-29 14:27:39 -0300

    Send env vars to /etc/environment file

--- env-scripts/docker-entrypoint.sh
+++ env-scripts/docker-entrypoint.sh
@@ -5,8 +5,16 @@ echo "export SHARED_DIR=\"$SHARED_DIR\"" >> /etc/environment
 echo "export SCRIPT_DIR=\"$SCRIPT_DIR\"" >> /etc/environment
 echo "export TZ=\"America/Sao_Paulo\"" >> /etc/environment
 #
-# used to force a specific year month download data. The format is: YYYY-MM-DD where DD is always 01
-echo "export FORCE_YEAR_MONTH=\"$FORCE_YEAR_MONTH\"" >> /etc/environment
+# if defined as env var, its used to force a specific year month to download data.
+if [[ -v FORCE_YEAR_MONTH ]]; then
+    # The format is: YYYY-MM-DD where DD is always 01
+    echo "export FORCE_YEAR_MONTH=\"$FORCE_YEAR_MONTH\"" >> /etc/environment
+fi;
+# if defined as env var, its used to force the execution every day.
+if [[ -v EVERY_DAY ]]; then
+    # Expected value is: yes to force or whatever
+    echo "export EVERY_DAY=\"$EVERY_DAY\"" >> /etc/environment
+fi;
 #
 # run cron in foreground
 cron -f
