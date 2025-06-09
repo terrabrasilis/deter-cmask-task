a297fa5968801fa56af9af19574635368c70dc88
André Carvalho <afacarvalho@yahoo.com.br>
2025-06-09 10:00:22 -0300

    Encode the password using urllib.parse.quote_plus

--- scripts/zonal-cloud.py
+++ scripts/zonal-cloud.py
@@ -23,6 +23,7 @@ import numpy as np
 from datetime import datetime
 import math
 import os
+import urllib
 from sqlalchemy import create_engine
 
 class ZonalCloud:
@@ -78,7 +79,8 @@ class ZonalCloud:
         # define connection with db based in biome
         self.con = psycopg2.connect(host=self.host, port=self.port, database=self.database, user=self.user, password=self.password)
         self.con.cursor().execute("SET application_name = 'ETL - DETER CMask Task';")
-        url = f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+        password_updated = urllib.parse.quote_plus(self.password)
+        url = f"postgresql+psycopg2://{self.user}:{password_updated}@{self.host}:{self.port}/{self.database}"
         self.engine = create_engine(url=url)
 
     def __getPreviousMonthFromMetadata(self):
